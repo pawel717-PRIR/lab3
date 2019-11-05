c509d10a3b1db453b03b6ba4e26c64cd9dea2736
pawel717 <psuchanicz@gmail.com>
2019-11-05 20:54:12 +0100

    Add standarization using cuda

--- c++/Main.cu
+++ c++/Main.cu
@@ -9,7 +9,6 @@ using namespace std;
 /**
  * compilation:
  * nvcc Main.cu Data.cu KnnAlgorithm.cu LoadCSV.cu Preprocessing.cu
- * g++ Main.cu Data.cu Data.h KnnAlgorithm.cu KnnAlgorithm.h LoadCSV.cu LoadCSV.h Preprocessing.cu Preprocessing.h
  * @return
  */
 int main() {
@@ -19,8 +18,8 @@ int main() {
     KnnAlgorithm knn = KnnAlgorithm();
 
     LoadCSV().myLoad("../dataset/mnist_train.csv", ourData.data, ourData.rows, ourData.columns);
-	preprocessing.Normalization(ourData.data, ourData.rows, ourData.columns);
-    //preprocessing.Standarization(ourData.data, ourData.rows, ourData.columns);
+	//preprocessing.Normalization(ourData.data, ourData.rows, ourData.columns);
+    preprocessing.Standarization(ourData.data, ourData.rows, ourData.columns);
     //knn.fit(&ourData, 75);
     //float accuracy = knn.predict();
     //printf("Accuracy knn: %f\n", accuracy);
--- c++/Preprocessing.cu
+++ c++/Preprocessing.cu
@@ -36,6 +36,38 @@ __global__ void cuda_normalization(float *data, int rows, int columns) {
     }
 }
 
+__global__ void cuda_standarization(float *data, int rows, int columns) {
+    int total_threads_count = blockDim.x * gridDim.x;
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    float var, ave, amo;
+
+    for (int i = tid+1; i < columns; i=i+total_threads_count) {
+        amo = 0, var = 0;
+        for (int j = 0; j < rows; ++j) {
+            amo = amo + *(data + (j * columns) + i);
+        }
+        ave  = amo / float(rows);
+
+        for (int j = 0; j < rows; ++j) {
+            float factor = *(data + (j * columns) + i) - ave;
+            var = var + (factor * factor);
+        }
+
+        if (var == 0) {
+            for (int j = 0; j < rows; j++) {
+                *(data + (j * columns) + i) = *(data + (j * columns) + i) / 255.;
+            }
+            continue;
+        }
+
+        float sd_reciprocal = 1./sqrt(var);
+
+        for (int j = 0; j < rows; j++) {
+            *(data + (j * columns) + i) = (*(data + (j * columns) + i) - ave) * sd_reciprocal;
+        }
+    }
+}
+
 Preprocessing::Preprocessing() {
 }
 
@@ -85,34 +117,41 @@ void Preprocessing::Normalization(float *data, int rows, int columns) {
 
 
 void Preprocessing::Standarization(float *data, int rows, int columns) {
-    int col_start_index = 0;
-    float var, ave, amo;
-
-    for (int i = col_start_index; i < columns - 1; ++i) {
-        amo = 0, var = 0;
-        for (int j = 0; j < rows; ++j) {
-            amo = amo + *(data + (j * columns) + i);
-        }
-        ave  = amo / float(rows);
-
-        for (int j = 0; j < rows; ++j) {
-            float factor = *(data + (j * columns) + i) - ave;
-            var = var + (factor * factor);
-        }
-
-        if (var == 0) {
-            for (int j = 0; j < rows; j++) {
-                *(data + (j * columns) + i) = *(data + (j * columns) + i) / 255.;
-            }
-            continue;
-        }
-
-        float sd_reciprocal = 1./sqrt(var);
-
-        for (int j = 0; j < rows; j++) {
-            *(data + (j * columns) + i) = (*(data + (j * columns) + i) - ave) * sd_reciprocal;
-        }
+    cudaDeviceProp cuda_properties; // information about gpu
+    HANDLE_ERROR(cudaGetDeviceProperties( &cuda_properties, 0));
+    int threads_count_per_block = cuda_properties.maxThreadsPerBlock; // use as many threads as possible on this device
+    if(threads_count_per_block > columns) {
+        threads_count_per_block = columns;
+    }
+    int blocks_count = (columns + threads_count_per_block - 1) / threads_count_per_block;
+    int max_blocks_count = cuda_properties.maxGridSize[0];
+    if(blocks_count > max_blocks_count) {
+        blocks_count = max_blocks_count;
     }
 
-    //printf("Czas obliczen standaryzacja: %f\n", MPI_Wtime() - startTime);
+    // copy data to compute into gpu device memory
+    float *cuda_data;
+    int data_size = sizeof(float) * rows * columns;
+    HANDLE_ERROR(cudaMalloc((void**)&cuda_data, data_size));
+    HANDLE_ERROR(cudaMemcpy(cuda_data, data, data_size, cudaMemcpyHostToDevice));
+
+    // measure time using cuda events
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+    // normalize
+    cuda_standarization<<<blocks_count, threads_count_per_block>>>(cuda_data, rows, columns);
+    cudaEventRecord(stop);
+
+    // copy computed data to from gpu device memory to host RAM
+    HANDLE_ERROR(cudaMemcpy(data, cuda_data, data_size, cudaMemcpyDeviceToHost));
+
+    // print elapsed time
+    cudaEventSynchronize(stop);
+    float elapsed_time = 0;
+    cudaEventElapsedTime(&elapsed_time, start, stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    printf("Czas obliczen standaryzacja: %f\n", elapsed_time/1000);
 }
